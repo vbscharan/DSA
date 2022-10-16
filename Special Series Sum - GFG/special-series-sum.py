0c9ffbbbc9ac2a3b68fa6d44c6982293ebac4202
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2022-10-16 21:30:59 +0530

    Added solution - LeetHub

---
+++ Special Series Sum - GFG/special-series-sum.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+class Solution:
+    def sumOfTheSeries (self, n):
+        # code here 
+        return n*(n+1)*(n+2)//6
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        n = int(input())
+        
+        ob = Solution()
+        print(ob.sumOfTheSeries(n))
+# } Driver Code Ends
