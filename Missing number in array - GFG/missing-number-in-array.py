25bdde6b177fcace5dbcc8d40184761a52a47610
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2022-10-26 21:48:53 +0530

    Added solution - LeetHub

---
+++ Missing number in array - GFG/missing-number-in-array.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+
+class Solution:
+    def MissingNumber(self,array,n):
+        # code here
+        k=n*(n+1)//2
+        s1=0
+        for i in array:
+            s1+=i
+        return k-s1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    s=Solution().MissingNumber(a,n)
+    print(s)
+# } Driver Code Ends
