2c60f11911599c06960a7f722ed14391f1e0f630
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2023-01-13 17:42:57 +0530

    Added solution - LeetHub

---
+++ Ishaan Loves Chocolates - GFG/ishaan-loves-chocolates.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    def chocolates(self, n : int, arr : List[int]) -> int:
+        # code here
+        return min(arr)
+        
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.chocolates(n, arr)
+        
+        print(res)
+        
+
+# } Driver Code Ends
