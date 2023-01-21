88451df1ff77646b2799a6f605199d34fdb5aa03
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2023-01-21 23:25:38 +0530

    Added solution - LeetHub

---
+++ Pangram Checking - GFG/pangram-checking.py
@@ -0,0 +1,49 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if a string is Pangram or not.
+    def checkPangram(self,s):
+        #code here
+        l=[0]*26
+        for i in s:
+            k=ord(i)
+            if k>=65 and k<=90:
+                l[k%65]+=1
+            elif k>=97 and k<=122:
+                l[k%97]+=1
+        for i in l:
+            if i==0:
+                return 0
+        return 1
+                
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        s=str(input())
+        obj = Solution()
+        if(obj.checkPangram(s)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
