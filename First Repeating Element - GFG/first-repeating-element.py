493c40a017af7aca0bd93e3429ac85d9a971d044
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2023-03-24 18:43:16 +0530

    Added solution - LeetHub

---
+++ First Repeating Element - GFG/first-repeating-element.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    #Function to return the position of the first repeating element.
+    def firstRepeated(self,arr, n):
+        
+        #arr : given array
+        #n : size of the array
+        dic={}
+        for i in arr:
+            dic[i]=dic.get(i,0)+1
+        for i in range(n):
+            if dic[arr[i]]>1:
+                return i+1
+        return -1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        
+        arr=[int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.firstRepeated(arr, n))
+# } Driver Code Ends
