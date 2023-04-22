b56fd1f7b82e9d526cd428d02d0b7e5cd170831a
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2023-04-22 17:48:50 +0530

    Added solution - LeetHub

--- Minimum indexed character - GFG/minimum-indexed-character.py
+++ Minimum indexed character - GFG/minimum-indexed-character.py
@@ -1,54 +1,34 @@
-#User function Template for python3
-
 class Solution:
-    
-    #Function to find the minimum indexed character.
-    def minIndexChar(self,Str, pat): 
-        #code here
-        dic={}
-        for i in range(len(Str)):
-            if dic.get(Str[i])!=None:
-                dic[Str[i]][0]=dic[Str[i]][0]+1
-                dic[Str[i]][1]=min(i,dic[Str[i]][1])
-            else:
-                dic[Str[i]]=[1,i]
-        k=len(Str);cut=0
-        for i in pat:
-            if dic.get(i)!=None:
-                if dic[i][1]<k:
-                    cut=1
-                    k=dic[i][1]
-        if cut:
-            return k
-        return -1
+    def printMinIndexChar(self, s, patt):
+		#Code here
+		dic={}
+		for i in range(len(s)):
+	        if dic.get(s[i])==None:
+		        dic[s[i]]=i
+		        #print(s[i],dic[s[i]])
         #print(dic)
-            
+	    nini=len(s)
+	    k=""
+	    for i in patt:
+	        #print(nini)
+	        if dic.get(i)!=None and dic[i]<nini:
+	            k=i
+	            nini=dic[i]
+        if k!="":
+            return k
+        else:
+            return '$'
+		    
 
 
 #{ 
  # Driver Code Starts
-#Initial Template for Python 3
-
-import atexit
-import io
-import sys
-
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
-
-@atexit.register
-
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
-
-if __name__=='__main__':
-    t = int(input())
-    for i in range(t):
-        s=str(input())
-        p=str(input())
-        obj = Solution()
-        ans = obj.minIndexChar(s,p)
-        print(ans)
+		
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+    	S = input().strip()
+    	patt = input().strip()
+    	obj = Solution()
+    	print(obj.printMinIndexChar(S, patt))
 # } Driver Code Ends
