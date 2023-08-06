a2006f39188f81fb1c8bc991d97eae647197e6ab
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2023-08-06 21:43:59 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.py
@@ -0,0 +1,43 @@
+#User function Template for python3
+
+from typing import List
+from queue import Queue
+class Solution:
+    #Function to return Breadth First Traversal of given graph.
+    def bfsOfGraph(self, V: int, adj: List[List[int]]) -> List[int]:
+        # code here
+        queue=[0]
+        visited=[0]*V
+        m=[]
+        while queue:
+            curr=queue.pop(0)
+            m.append(curr)
+            for i in adj[curr]:
+                if not visited[i]:
+                    visited[i]=1
+                    #m.append(curr)
+                    queue.append(i)
+                    
+        return m   
+
+
+#{ 
+ # Driver Code Starts
+
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		V, E = map(int, input().split())
+		adj = [[] for i in range(V)]
+		for _ in range(E):
+			u, v = map(int, input().split())
+			adj[u].append(v)
+		ob = Solution()
+		ans = ob.bfsOfGraph(V, adj)
+		for i in range(len(ans)):
+		    print(ans[i], end = " ")
+		print()
+        
+
+# } Driver Code Ends
