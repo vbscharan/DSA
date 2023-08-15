50c2cae51eb75e9a2f486a277f502539423dc743
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2023-08-15 13:57:35 +0530

    Added solution - LeetHub

---
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.py
@@ -0,0 +1,75 @@
+#User function Template for python3
+
+from typing import List
+
+class Solution:
+    def shortestPath(self, n : int, m : int, edges : List[List[int]]) -> List[int]:
+        distance=[1000000]*n
+        distance[0]=0
+        for count in range(n):
+            for edge in edges:
+                if distance[edge[0]]+edge[2]<distance[edge[1]]:
+                    distance[edge[1]]=distance[edge[0]]+edge[2]
+        for count in range(n):
+            for edge in edges:
+                if distance[edge[0]]+edge[2]<distance[edge[1]]:
+                    return -1
+        for i in range(len(distance)):
+            if distance[i]==1000000:
+                distance[i]=-1
+        return distance
+
+
+#{ 
+ # Driver Code Starts
+
+#Initial Template for Python 3
+
+from typing import List
+
+
+
+
+class IntMatrix:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n,m):
+        matrix=[]
+        #matrix input
+        for _ in range(n):
+            matrix.append([int(i) for i in input().strip().split()])
+        return matrix
+    def Print(self,arr):
+        for i in arr:
+            for j in i:
+                print(j,end=" ")
+            print()
+
+
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n,m=map(int,input().split())
+        
+        
+        edges=IntMatrix().Input(m, 3)
+        
+        obj = Solution()
+        res = obj.shortestPath(n, m, edges)
+        
+        IntArray().Print(res)
+# } Driver Code Ends
