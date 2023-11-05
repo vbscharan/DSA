75a4eca9740313c9b408d1e617a22f39a77f837a
Bhanu Sri Charan Vellanki <72484251+vbscharan@users.noreply.github.com>
2023-11-05 15:16:56 +0530

    Added solution - LeetHub

--- Maximum Profit - GFG/maximum-profit.py
+++ Maximum Profit - GFG/maximum-profit.py
@@ -3,6 +3,7 @@
 class Solution:
     def maxProfit(self, K, N, A):
         # code here
+        '''
         dp=[[[-1 for _ in range(K)]for _ in range(2)] for _ in range(N)]
         def calp(ind,buy,k):
             if k<0 or ind==N:
@@ -16,7 +17,17 @@ class Solution:
             dp[ind][buy][k]=p
             return dp[ind][buy][k]
         return calp(0,1,K-1)
-
+        '''
+        dp=[[[0 for _ in range(K+1)]for _ in range(2)] for _ in range(N+1)]
+        for ind in range(N-1,-1,-1):
+            for buy in range(2):
+                for k in range(1,K+1):
+                    if buy:
+                        p= max(-A[ind]+dp[ind+1][0][k],dp[ind+1][1][k])
+                    else:
+                        p= max(A[ind]+dp[ind+1][1][k-1],dp[ind+1][0][k])
+                    dp[ind][buy][k]=p
+        return dp[0][1][K]
 
 #{ 
  # Driver Code Starts
